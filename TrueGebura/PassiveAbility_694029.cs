a971fad72e63bf50e6abc203f542fad83fc100bc
Hashim Qureshi <42122270+CasualHonest@users.noreply.github.com>
2021-05-16 12:55:49 -0400

    Add source code

---
+++ TrueGebura/PassiveAbility_694029.cs
@@ -0,0 +1,22 @@
+﻿namespace TrueGebura
+{
+    public class PassiveAbility_694029 : PassiveAbilityBase
+    {
+        public override void OnWaveStart()
+        {
+            owner.allyCardDetail.DrawCards(4);
+        }
+
+        public override void OnAfterRollSpeedDice()
+        {
+            CheckDeckForRMPages();
+        }
+
+        public void CheckDeckForRMPages()
+        {
+            foreach (BattleDiceCardModel model in owner.allyCardDetail.GetHand())
+                if ((model.GetTextId() == 607003 || model.GetTextId() == 607004 || model.GetTextId() == 607005) && model.CurCost >= 2)
+                    model.SetCurrentCost(model.CurCost - 1);
+        }
+    }
+}
